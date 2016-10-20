f919e92306bea2fa6f8f5deeb6ee2778bef90f5d
Gene Der Su <e870252314@yahoo.com.tw>
2016-10-20 15:31:01 -0700

    feat: added signup feature
    Signup page check the length of the username and password, verify
    whether password typed correctly, and whether the username has exist
    before. If all fields has no error, it’ll store the values into the
    user table. The username and password are hashed with different salt
    before store into the database. All fields are url encoded before
    retrieve.

--- flaskapp.py
+++ flaskapp.py
@@ -1,16 +1,19 @@
 from collections import Counter
 import csv
 import sqlite3
+import urllib
+import hmac
 
-from flask import Flask, request, g, render_template
+from flask import Flask, request, g, render_template, redirect, url_for
 
-DATABASE = '/var/www/html/flaskapp/natlpark.db'
+#DATABASE = '/var/www/html/flaskapp/adserver.db'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 def connect_to_database():
-    return sqlite3.connect(app.config['DATABASE'])
+    #return sqlite3.connect(app.config['DATABASE'])
+    return sqlite3.connect('adserver.db')
 
 def get_db():
     db = getattr(g, 'db', None)
@@ -18,21 +21,60 @@ def get_db():
         db = g.db = connect_to_database()
     return db
 
+secret1 = "GeneDerSu@gofind.ai-0912u90jds"
+secret2 = "lmcxvlcxm10-980ucvjn2l3mrec0jl"
+def make_secure_username(val):
+    return hmac.new(secret1, val).hexdigest()
+def make_secure_password(val):
+    return hmac.new(secret2, val).hexdigest()
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = urllib.quote_plus(request.form['username'])
+        password = urllib.quote_plus(request.form['password'])
+        verify = urllib.quote_plus(request.form['verify'])
+        
+        # checking for valid entries
+        if len(username) > 250:
+            return render_template("signup.html",
+                                   error_username='length of username > 250')
+        if len(username) == 0:
+            return render_template("signup.html",
+                                   username=urllib.unquote_plus(username),
+                                   error_username='no username entered')
+        if len(password) > 250:
+            return render_template("signup.html",
+                                   error_password='length of password > 250')
+        if len(password) == 0:
+            return render_template("signup.html",
+                                   error_password='no password entered')
+        if password != verify:
+            return render_template("signup.html",
+                                   error_varify='passwords do not match')
+        secured_username = make_secure_username(username)
+        secured_password = make_secure_password(password)
+        rows = select_query("""SELECT COUNT(*) FROM user WHERE username = ?""",
+                            [secured_username])
+        print "hello:", rows, len((secured_username, secured_password))
+        if rows[0][0] != 0:
+            return render_template("signup.html",
+                                   error_username='username already exist')
+
+        insert_query("""INSERT INTO user (username,password) VALUES (?,?)""",
+                      [secured_username, secured_password])
+
+        return redirect(url_for('welcomeback'))
+    else:
+        return render_template("signup.html")
+
 @app.route('/login')
 def login():
     return render_template("login.html")
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POS':
-        pass
-    else:
-        return render_template("signup.html")
 
 @app.route('/welcomeback')
 def welcomeback():
@@ -74,20 +116,23 @@ def sortby(state):
                          [state.title()])
     return '<br>'.join(str(row) for row in rows)
 
-def hello_world():
-  return 'Hello from Flask!'
-
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
 
-def execute_query(query, args=()):
+def select_query(query, args=()):
     cur = get_db().execute(query, args)
     rows = cur.fetchall()
     cur.close()
     return rows
+
+def insert_query(query, args=()):
+    conn = get_db()
+    conn.execute(query, args)
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
   app.run()
